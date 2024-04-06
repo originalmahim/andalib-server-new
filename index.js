a860c79064613aa40dca4a5eebc57da5cd3a2697
originalmahim <tareqazizmahim@gmail.com>
2024-04-06 22:26:06 +0600

    created patche api for pending orders

--- index.js
+++ index.js
@@ -71,6 +71,19 @@ app.patch('/cancelled/:id', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+app.patch('/pending/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: id };
+    const update = { $set: { status: "Pending" } }; 
+    const options = { returnOriginal: false }; 
+    const result = await ordersFile.updateOne(query, update, options);
+    res.send(result)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 app.get('/', (req, res) => {
